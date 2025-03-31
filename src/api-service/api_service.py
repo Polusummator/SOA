f01d933d5b64f494c94514a1a12089df379ca711
Alexander Reznichenko <avreznichenko_2@edu.hse.ru>
2025-03-31 21:43:57 +0300

    Add routers for api-service

--- src/api-service/api_service.py
+++ src/api-service/api_service.py
@@ -1,31 +1,10 @@
 from fastapi import FastAPI, Request, Response, HTTPException
 import uvicorn
-import httpx
 
-URL_USER_SERVICE = "http://user-service:5000"
+from routers import router_users, router_posts
 
 app = FastAPI()
-
-@app.api_route("/{path:path}", methods=["GET", "POST", "PUT"])
-async def user(path: str, request: Request):
-    url = f"{URL_USER_SERVICE}/{path}"
-    async with httpx.AsyncClient() as client:
-        try:
-            response = await client.request(
-                request.method,
-                url,
-                headers=dict(request.headers),
-                content=await request.body(),
-                params=request.query_params,
-            )
-            return Response(
-                content=response.content,
-                status_code=response.status_code,
-                headers=response.headers,
-            )
-        except httpx.RequestError as e:
-            raise HTTPException(status_code=500, detail=str(e))
-
+app.include_router(router_users.router)
 
 if __name__ == '__main__':
     uvicorn.run(app, host='0.0.0.0', port=8000)
---
+++ src/api-service/config.py
@@ -0,0 +1,5 @@
+
+
+URL_USER_SERVICE = "http://user-service:5000"
+URL_POST_SERVICE = "http://post-service:5000"
+
